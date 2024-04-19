5999b76806c6b9276899b7f16fbd924d55e03820
xavier <villax989@icloud.com>
2024-04-18 22:06:16 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function employee() {
+const employee={
+name:"xavier",
+streetAddress:"644 main st",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{const newObj = { ...employee }
+newObj[key] = value
+return newObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee[key] = value
+return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = { ...employee }   
+    delete newObj[key]
+    return newObj
+}
 
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
 }
